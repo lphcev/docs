6a54946cc9db00cc84be0575e2c99a441c737af0
lphcev <lphcev@163.com>
2026-06-13 12:29:12 +0800

    .

---
+++ assets/content_unorganized_other_index.md.K5016yMa.js
@@ -0,0 +1,12 @@
+import{_ as a,o as s,c as e,a2 as p}from"./chunks/framework.uPtiMkzH.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"content/unorganized/other/index.md","filePath":"content/unorganized/other/index.md"}'),t={name:"content/unorganized/other/index.md"};function i(l,n,o,r,c,d){return s(),e("div",null,[...n[0]||(n[0]=[p(`<h2 id="题库" tabindex="-1">题库 <a class="header-anchor" href="#题库" aria-label="Permalink to &quot;题库&quot;">​</a></h2><p>path 定义成变量, 使用 []</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>const path = &#39;/unorganized/other/&#39;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>export default {</span></span>
+<span class="line"><span>  sidebar: {</span></span>
+<span class="line"><span>    [path]: [</span></span>
+<span class="line"><span>      {</span></span>
+<span class="line"><span>        text: &#39;Unorganized Other&#39;,</span></span>
+<span class="line"><span>        items: [{ text: &#39;Resume&#39;, link: &#39;/unorganized/other/resume&#39; }],</span></span>
+<span class="line"><span>      },</span></span>
+<span class="line"><span>    ],</span></span>
+<span class="line"><span>  },</span></span>
+<span class="line"><span>} as any</span></span></code></pre></div>`,3)])])}const u=a(t,[["render",i]]);export{_ as __pageData,u as default};
